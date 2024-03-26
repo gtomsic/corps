4b079361d16239aa692be8e115dcce569066b80e
Gabriel Tomsic <tomsicgabriel@gmail.com>
2024-03-25 22:02:56 -0500

    Corp basic application complete

---
+++ src/app/reliability/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/hero";
+
+export default function ReliabilityPage() {
+  return (
+    <Hero
+      imgData="/reliability.jpg"
+      imgAlt="welding"
+      title="Super hight realibility hosting"
+    />
+  );
+}
---
+++ src/app/scale/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/hero";
+
+export default function ScalePage() {
+  return (
+    <Hero
+      imgData="/scale.jpg"
+      imgAlt="steal factory"
+      title="Scale your app to infinity"
+    />
+  );
+}
